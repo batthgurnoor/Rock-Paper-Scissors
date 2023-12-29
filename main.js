a8ad1784734ab98c4563398da07ff992abb0e173
batthgurnoor <gurnoorbatth10@gmail.com>
2023-12-28 23:49:13 -0800

    Added Results page and then result audios

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
 const choice = ["rock", "paper", "scissors"];
+const content = [
+  "MACHINES ARE THREATING TO TAKE OVER THE WORLD....",
+  "UNLESS YOU BEAT THEM AT A GAME OF ROCK PAPER SCISSORS FIRST TO 5.....",
+  "DO YOU HAVE WHAT IT TAKES TO SAVE THE WORLD?",
+];
+const playerWinMessage = [
+  "CONGRATULATIONS",
+  "DEFENDER OF HUMANITY!",
+  "YOU'VE SAVED US ALL FROM THE DIGITAL DOOM!",
+];
+const computerWinMessage = [
+  "THE MACHINES CLAIM VICTORY!",
+  "GATHER YOUR COURAGE FOR ANOTHER ROUND",
+  "BRAVE HUMAN!",
+];
+const tieMessage = [
+  "EQUAL FORCES HAVE MET!",
+  " NEITHER MAN NOR MACHINE TAKES THE CROWN TODAY",
+  "ANOTHER ROUND?",
+];
+
+var message = document.getElementById("intro_text");
+let currentIndex = 0;
+let charIndex = 0;
+
+let output = document.getElementById("intro_text");
 var bodyContainer = document.getElementById("body_container");
 var resultContainer = document.getElementById("result_page");
+let resultText = document.getElementById("outcome");
+var gameCounter = 0;
+var playWinCount = 0;
+var compWinCount = 0;
+var tieCount = 0;
+
 function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * 1000000) % choice.length;
   const randomOption = choice[randomIndex];
 
   return randomOption;
 }
-var gameCounter = 0;
-var playWinCount = 0;
-var compWinCount = 0;
-var tieCount = 0;
 
 function playround(playerSelection) {
   playerSelection = playerSelection.toString().toLowerCase();
@@ -33,14 +61,6 @@ function playround(playerSelection) {
     ).innerHTML = `DAMN IT! Machines win this round. ${computerSelection} beats ${playerSelection}`;
     return "loss";
   }
-}
-
-function declareWinner() {
-  bodyContainer.classList.add("fadeOut"); // Apply the animation
-  setTimeout(function () {
-    bodyContainer.innerHTML = ""; // Clear content after animation completes
-  }, 1000);
-  resultContainer.style.visibility = "visible";
 }
 
 function playGame(event) {
@@ -66,20 +86,9 @@ function playGame(event) {
   }
 }
 
-const content = [
-  "MACHINES ARE THREATING TO TAKE OVER THE WORLD....",
-  "UNLESS YOU BEAT THEM AT A GAME OF ROCK PAPER SCISSORS FIRST TO 5.....",
-  "DO YOU HAVE WHAT IT TAKES TO SAVE THE WORLD?",
-];
-
-var message = document.getElementById("intro_text");
-let currentIndex = 0;
-let charIndex = 0;
-let output = document.getElementById("intro_text");
-
 function showContent() {
   var x = document.getElementById("actualContent");
-  x.style.opacity = 1;
+  x.style.visibility = "visible";
   x.style.transition = "2s";
 }
 let lastTime = 0;
@@ -107,3 +116,35 @@ function displayCharacters(time) {
   }
 }
 requestAnimationFrame(displayCharacters);
+
+function declareWinner() {
+  bodyContainer.classList.add("fadeOut");
+  setTimeout(function () {
+    bodyContainer.innerHTML = "";
+  }, 1000);
+  resultContainer.classList.add("fadeIn");
+  setTimeout(function () {
+    resultContainer.style.visibility = "visible";
+    displayWinner();
+  }, 2000);
+}
+
+function displayWinner() {
+  let content1;
+  var audio;
+  if (playWinCount > compWinCount && playWinCount >= tieCount) {
+    content1 = playerWinMessage;
+    audio = new Audio("win.wav");
+  } else if (playWinCount < compWinCount && compWinCount >= tieCount) {
+    content1 = computerWinMessage;
+    audio = new Audio("loss.mp3");
+  } else {
+    audio = new Audio("tie.wav");
+    content1 = tieMessage;
+  }
+  audio.play();
+  document.getElementById("reload_button").style.visibility = "visibile";
+  document.getElementById("outcome").innerHTML = content1[0];
+  document.getElementById("outcome_inner").innerHTML = content1[1];
+  document.getElementById("outcome_last").innerHTML = content1[2];
+}
